f3116bf3dbb01c7d5e40bb558854b0d803f9c28a
mickey390390 <chanwaileong390@gmail.com>
2022-03-02 00:18:40 +0800

    testing

--- test_project/test.py
+++ test_project/test.py
@@ -1,15 +1,12 @@
 import math
 import os
 import sys
-
 import requests
 
 r = requests.get("https://coreyms.com")
 print(r.status_code)
 print(r.ok)
-
 print(sys.version)
 print(sys.executable)
 print("tstst")
-
 print("branch1")
